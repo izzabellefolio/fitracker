3e599c85eea536b25c92f424837ee98223eb81b3
Izza Belle Lelis <izzabelle.folio@gmail.com>
2025-12-09 23:11:14 +0800

    update commit

--- src/app/core/services/premade-routine.service.ts
+++ src/app/core/services/premade-routine.service.ts
@@ -11,6 +11,7 @@ export class PremadeRoutineService {
   constructor(private http: HttpClient) {}
 
   getPremadeRoutines(): Observable<Routine[]> {
-    return this.http.get<Routine[]>('data/premade-routines.json');
+    return this.http.get<Routine[]>(`${window.location.pathname}data/premade-routines.json`);
   }
+
 }
